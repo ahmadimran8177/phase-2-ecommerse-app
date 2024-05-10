95f926f9a2951af5d6ab640fe44c02f251ca895a
ahmadimran8177 <ahmadImran8177@gmail.com>
2024-05-11 01:44:14 +0500

    Update page.jsx

--- app/error/page.jsx
+++ app/error/page.jsx
@@ -6,9 +6,9 @@ const IncorrectCredentials = () => {
             <h2>Oops! There seems to be a problem.</h2>
             <p>The username or password you entered is incorrect. Please try again.</p>
             <ul>
-                <li>Make sure you've typed your username and password correctly.</li>
+                <li>Make sure you&apos;ve typed your username and password correctly.</li>
                 <li>Check for caps lock being on (passwords are case-sensitive).</li>
-                <li>If you've forgotten your password, you can click "Forgot Password" below to reset it.</li>
+                <li>If you&apos;ve forgotten your password, you can click &quot;Forgot Password&quot; below to reset it.</li>
             </ul>
             <a href='/'>Back to home</a>
         </div>
